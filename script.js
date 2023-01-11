356d941ebb517163ce8396bac0086f0b5ab5e4e7
Odi000 <mandimandi.ro@gmail.com>
2023-01-11 14:36:59 +0100

    Solved some problems but mostly created a mess

--- script.js
+++ script.js
@@ -1,30 +1,69 @@
+//GamePlay Module
 (()=> {
     const cells = [...document.querySelectorAll('.cell')];
+    const player_1 = Player(
+        prompt('Please enter your name:','Andy'),'x'
+        );
+    const player_2 = Player('PC','o');
     const winningComb = [
-        [1,2,3],
-        [4,5,6],
-        [7,8,9],
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
         [1,4,7],
         [2,5,8],
-        [3,6,9],
-        [1,5,9],
-        [3,5,7]
+        [0,4,8],
+        [2,4,6]
     ];
 
-    let pTurn = 0;
+    let pTurn = player_1;
     cells.forEach(cell => cell.onclick = () => {
-        const X = document.createElement('img');
-        const O = document.createElement('img');
-        X.src = "./images/x.svg";
-        O.src = "./images/circle.svg";
-        O.style.width = "90%";
         if(cell.firstElementChild) return;
-        if(pTurn === 0){
-            cell.appendChild(O);
-            pTurn = 1;
+        if(pTurn === player_1){
+            cell.appendChild(player_1.sign());
+            pTurn = player_2;
+            (() => {
+                winningComb.forEach(comb => {
+                    for(i in comb) {
+                        if(!(cells[comb[i]].firstElementChild)) return;
+                        // console.log(cells[comb[i]].firstElementChild);
+                    }
+                    const el_1 = cells[comb[0]].firstElementChild.id;
+                    const el_2 = cells[comb[1]].firstElementChild.id;
+                    const el_3 = cells[comb[2]].firstElementChild.id;
+                    const asnwer = el_1 == el_2 && el_1 == el_3? true: false;
+                    console.log(asnwer);
+                })
+            })()
         } else {
-            cell.appendChild(X);
-            pTurn = 0;
+            cell.appendChild(player_2.sign());
+            pTurn = player_1;
+            (() => {
+                winningComb.forEach(comb => {
+                    for(i in comb) {
+                        if(!(cells[comb[i]].firstElementChild)) return;
+                        // console.log(cells[comb[i]].firstElementChild);
+                    }
+                    const el_1 = cells[comb[0]].firstElementChild.id;
+                    const el_2 = cells[comb[1]].firstElementChild.id;
+                    const el_3 = cells[comb[2]].firstElementChild.id;
+                    const asnwer = el_1 == el_2 && el_1 == el_3? true: false;
+                    return asnwer;
+                })
+            })()
         }
     })
 })()
+
+//Create Player Factory Function
+function Player(name, signature) {
+    let points = 0;
+    const sign = function () {
+            const xo = document.createElement('img');
+            xo.src = `./images/${signature}.svg`;
+            xo.id = signature;
+            if(signature == 'o') xo.style.width = '90%';
+            return xo;
+    }
+    return {name, points, sign};
+}
